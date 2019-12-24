17ccb22545c814ef947319e78ef054f014dc6240
Максим Лобин <lobinmax@yandex.ru>
2019-12-25 02:50:02 +0700

    25.12.2019 - frLogin

---
+++ AutoSalon/WinForms/frLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace AutoSalon.WinForms
+{
+    public partial class frLogin : DevExpress.XtraEditors.XtraForm
+    {
+        public frLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
